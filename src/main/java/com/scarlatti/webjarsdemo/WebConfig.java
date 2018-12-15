79ebfdb88e68a3c9843bb2d58f93a92992be3785
alessandroscarlatti <violanotesnoextras@gmail.com>
2018-12-15 09:28:33 -0600

    initial commit

---
+++ src/main/java/com/scarlatti/webjarsdemo/WebConfig.java
@@ -0,0 +1,23 @@
+package com.scarlatti.webjarsdemo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * ______    __                         __           ____             __     __  __  _
+ * ___/ _ | / /__ ___ ___ ___ ____  ___/ /______    / __/______ _____/ /__ _/ /_/ /_(_)
+ * __/ __ |/ / -_|_-<(_-</ _ `/ _ \/ _  / __/ _ \  _\ \/ __/ _ `/ __/ / _ `/ __/ __/ /
+ * /_/ |_/_/\__/___/___/\_,_/_//_/\_,_/_/  \___/ /___/\__/\_,_/_/ /_/\_,_/\__/\__/_/
+ * Saturday, 12/15/2018
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/webjars/**")
+            .addResourceLocations("/webjars/")
+            .resourceChain(false);
+    }
+}
---
+++ src/test/java/com/scarlatti/webjarsdemo/WebjarsDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.scarlatti.webjarsdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebjarsDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
